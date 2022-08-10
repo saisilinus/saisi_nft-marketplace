a2b15e1bb214a199cf81f2c6ecce702970d7bd60
saisilinus <saisilinus19@gmail.com>
2022-08-10 12:37:06 +0300

    feat: initial commit

---
+++ pages/index.tsx
@@ -0,0 +1,9 @@
+import type { NextPage } from 'next';
+
+const Home: NextPage = () => (
+  <div className="container mx-auto p-6 items-center">
+    <h1 className="text-3xl font-bold underline">Hello World!</h1>
+  </div>
+);
+
+export default Home;
