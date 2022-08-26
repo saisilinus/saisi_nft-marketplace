8076137df842fb978a28fa409242ebf77a78ab54
saisilinus <saisilinus19@gmail.com>
2022-08-26 16:47:11 +0300

    feat: add testnet warning

--- pages/index.tsx
+++ pages/index.tsx
@@ -106,10 +106,15 @@ const Home: NextPage = () => {
   return (
     <div className="flex justify-center sm:px-4 p-12">
       <div className="w-full minmd:w-4/5">
+        <h1 className="font-poppins dark:text-red-500 text-nft-black-1 text-xl minlg:text-4xl font-semibold ml-4 xs:ml-0 mb-8">This smart contract is deployed to the Polygon Mumbai Testnet!</h1>
         <Banner
           parentStyles="justify-start mb-7 h-72 sm:h-60 p-12 xs:p-4 xs:h-44 rounded-3xl"
           childStyles="md:text-4xl sm:text-2xl xs:text-xl text-left"
-          name={(<>Discover, collect, and sell <br />extraordinary NTFs</>)}
+          name={(
+            <>
+              Discover, collect, and sell <br />extraordinary NTFs
+            </>
+          )}
         />
 
         {!isLoading && !nfts.length
